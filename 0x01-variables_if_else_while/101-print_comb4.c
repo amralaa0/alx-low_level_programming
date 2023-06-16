ecfcd19454be92547e7fd78816b02df748c39feb
Amr <amrmora933@gmail.com>
2023-06-16 16:08:43 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,10 +21,7 @@ int main(void)
 			digit3 = 0;
 			while (digit3 <= 9)
 			{
-			if (digit1 != digit2
-				&& digit1 < digit2
-						&& digit2 < digit3
-						&& digit2 != digit3)
+			if (digit1 != digit2 && digit1 < digit2 && digit2 < digit3&& digit2 != digit3)
 			{
 				putchar(digit1 + 48);
 				putchar(digit2 + 48);
@@ -35,7 +32,8 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-				digit3++;
+			}
+			digit3++;
 			}
 			digit2++;
 			}
