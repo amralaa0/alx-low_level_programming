61e927dd0a883a1f2d2bb11ac2523f16fafe22a2
Amr <amrmora933@gmail.com>
2023-06-16 08:38:23 -0700

    this is the first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,21 @@
 /**
  * main - entry point
  *
- * Description: prints the alphabet in lowercase, followed by a new line
+ * Description: print all the alphabet letter
  *
  * Return : 0 (success)
 */
+
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
+
 	return (0);
 }
