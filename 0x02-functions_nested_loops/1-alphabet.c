137be53ccf27a72f0584772a222817d325c89121
Amr <amrmora933@gmail.com>
2023-06-20 09:13:04 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description:
+*
+* Return: Always 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
