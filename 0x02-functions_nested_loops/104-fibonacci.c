826c9059473288d35f09ea7dabbbe8e78f9a77f0
Amr <amrmora933@gmail.com>
2023-06-20 15:10:42 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,8 +18,8 @@ int main(void)
 		if (f1o > 0)
 		{
 			printf("%lu", f1o);
-			initial0s = numLength(mx) - 1 - numLength(f1);
 		}
+		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%d", 0);
