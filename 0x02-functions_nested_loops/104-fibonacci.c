e31281b031b20c4ded438fc81fc6e5319ac274f1
Amr <amrmora933@gmail.com>
2023-06-20 15:16:20 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,6 +8,20 @@
  * Return: Always 0 (Success)
 */
 
+int numlength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
 int main(void)
 {
 	int count, initial0s;
