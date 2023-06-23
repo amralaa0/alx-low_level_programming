69f32dadeefd5b430b3fa4c8da430857469f012a
Amr <amrmora933@gmail.com>
2023-06-23 05:16:14 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,17 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+		printf(" ");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
+		printf(" ");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
+		printf(" ");
 		else
-			printf("%d ", num);
+			printf("%d", num);
+		printf(" ");
 	}
 
 	return (0);
