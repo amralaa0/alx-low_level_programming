b5c89d2f7cda489fd22c4d5229b7fbb6a772f116
Amr <amrmora933@gmail.com>
2023-06-23 05:33:21 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,9 +18,11 @@ void print_diagonal(int n)
 		for (i = 1; i <= n; i++)
 		{
 			for (j = 1; j <= i; j++)
+			{
 				_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
+				_putchar(92);
+				_putchar('\n');
+			}
 		}
 	}
 }
