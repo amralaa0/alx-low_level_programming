afe436bd47ce8d919254c2bf856e4db904bd00e0
Amr <amrmora933@gmail.com>
2023-06-29 17:52:09 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-
 /**
- * leet - function that encodes a string into 1337
- * @str: character to be checked
- * Return: if seperator return 1. Otherwise return 0;
-*/
-char *leet(char *str)
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 (success)
+ */
+int main(void)
 {
-	int count = 0, i = 0;
-	char *letters = "aeotlAEOTL";
-	char *nums = "4307143071";
+	int n;
+	int a[5];
+	int *p;
 
-	while (str[count] != '\0')
-	{
-		i = 0;
-		while (letters[i] != '\0')
-		{
-			if (str[count] == letters[i])
-				str[count] = nums[i];
-			i++;
-		}
-		count++;
-	}
+	a[2] = 1024;
+	p = &n;
 
-	return (str);
+	/**
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
